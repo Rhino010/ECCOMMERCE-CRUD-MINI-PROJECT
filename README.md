24cca927bd63a865c3ab0705b5d92de723081d4b
Ryan Boll <bollr38@hotmail.com>
2024-12-29 20:23:45 -0600

    order total updated

---
+++ README.md
@@ -0,0 +1,9 @@
+***WELCOME TO THE ECOMMERCE MINI PROJECT***
+
+-OVERVIEW-
+
+This project has been designed to utilize Postman to create a database to hold information about Customers including (name, email, date of birth, and addresses), 
+User Accounts including (username and passwords), Orders including (order dates, status of order, date order is closed, and the total of the order), and Products
+including (name/description, the par inventory to be kept on hand, the current inventory, and the product price.)
+
+-User Routes-
--- app.py
+++ app.py
@@ -44,11 +44,11 @@ class OrderSchema(ma.Schema):
 class ProductSchema(ma.Schema):
     name = fields.String(required=True)
     par_inventory = fields.Integer(required=True)
-    current_invenotory = fields.Integer(required=True)
+    current_inventory = fields.Integer(required=True)
     price = fields.Float(required=True, validate=validate.Range(min=0))
 
     class Meta:
-        fields = ("name", "par_inventory", "current_inventory", "price")
+        fields = ("name", "par_inventory", "current_inventory", "price", "id")
 
 customer_schema = CustomerSchema()
 customers_schema = CustomerSchema(many=True)
@@ -73,21 +73,16 @@ class Customer(db.Model):
     address = db.Column(db.String(350), nullable=False)
     account_id = db.Column(db.Integer, db.ForeignKey('CustomerAccounts.id'))
 
-customer_account = db.Table("Customer_Account",
-                            db.Column('customer_id', db.Integer, db.ForeignKey('Customers.id'), primary_key=True),
-                            db.Column('account_id', db.Integer, db.ForeignKey('CustomerAccounts.id'), primary_key=True)
-                            )
 
 class CustomerAccount(db.Model):
     __tablename__ = "CustomerAccounts"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
-    order_id = db.Column(db.Integer, db.ForeignKey('Orders.id'))
     customer_id = db.Column(db.Integer, db.ForeignKey('Customers.id'))
 
 customer_orders = db.Table('Customer_Orders',
-                           db.Column('account_id', db.Integer, db.ForeignKey('CustomerAccounts.id'), primary_key=True),
+                           db.Column('product_id', db.Integer, db.ForeignKey('Products.id'), primary_key=True),
                            db.Column('order_id', db.Integer, db.ForeignKey('Orders.id'), primary_key=True)
                            )
 
@@ -97,15 +92,13 @@ class Order(db.Model):
     order_date = db.Column(db.Date, nullable=False)
     status = db.Column(db.String(350), nullable=False)
     close_date = db.Column(db.Date)
-    order_total = db.Column(db.Float, nullable=False)
-    product_id = db.Column(db.Integer, db.ForeignKey('Products.id'))
+    order_total = db.Column(db.Float, nullable=False, default=0.0)
     account_id = db.Column(db.Integer, db.ForeignKey('CustomerAccounts.id'))
-    account_orders = db.relationship('CustomerAccount', backref='account_orders')
 
-product_order = db.Table('Product_Order', 
-                         db.Column('order_id', db.Integer, db.ForeignKey('Orders.id'),primary_key=True),
-                         db.Column('product_id', db.Integer, db.ForeignKey('Products.id'), primary_key=True)
-                         )
+    products = db.relationship('Product', secondary=customer_orders, backref=db.backref('orders', lazy=True))
+
+    def calculate_total(self):
+        self.order_total = sum(product.price for product in self.products)
 
 class Product(db.Model):
     __tablename__ = "Products"
@@ -114,12 +107,231 @@ class Product(db.Model):
     par_inventory = db.Column(db.Integer, nullable=False)
     current_inventory = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float, nullable=False)
-    product_orders = db.relationship('Order', secondary=product_order, backref=db.backref('orders_of_products'))
+    product_orders = db.relationship('Order', secondary=customer_orders, backref=db.backref('orders_of_products'))
 
 
 # Creates tables in database
 with app.app_context():
     db.create_all()
+
+# Routes Separation ----------Preliminary Customer Routes-----------------------------------------
+
+@app.route('/customers', methods=['GET'])
+def get_customers():
+    customers = Customer.query.all()
+    return customers_schema.jsonify(customers)
+
+@app.route('/customers', methods=['POST'])
+def add_customer():
+    try:
+        # Validate and desiarlize input
+        customer_data = customer_schema.load(request.json)
+    
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    new_customer = Customer(
+                            name = customer_data['name'],
+                            email = customer_data['email'],
+                            date_of_birth = customer_data['date_of_birth'],
+                            address = customer_data['address']
+                            )
+    
+    db.session.add(new_customer)
+    db.session.commit()
+    return jsonify({'message': "New customer added successfully"}), 201
+
+
+@app.route('/customers/<int:id>', methods=['PUT'])
+def update_customer(id):
+    customer = Customer.query.get_or_404(id)
+    try:
+        customer_data = customer_schema.load(request.json)
+
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    customer.name = customer_data['name']
+    customer.email = customer_data['email']
+    customer.date_of_birth = customer_data['date_of_birth']
+    customer.address = customer_data['address']
+    db.session.commit()
+    return jsonify({'message': 'Customer details updated successfully'}), 200
+
+
+@app.route('/customers/<int:id>', methods=['DELETE'])
+def delete_customer(id):
+    customer = Customer.query.get_or_404(id)#Makes sure customer exists
+    db.session.delete(customer)
+    db.session.commit()
+    return jsonify({'message': 'Customer removed successfully.'}), 200
+
+# Routes Separation--------------------------Preliminary Customer Account Routes--------------------------------
+
+@app.route('/customer_accounts', methods=['GET'])
+def get_accounts():
+    accounts = CustomerAccount.query.all()
+    return customers_account_schema.jsonify(accounts)
+
+@app.route('/customer_accounts', methods=['POST'])
+def add_customer_account():
+    try:
+        customer_account_data = customer_account_schema.load(request.json)
+
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    new_customer_account = CustomerAccount(
+                                            username = customer_account_data['username'],
+                                            password = customer_account_data['password']
+                                            )
+    
+    db.session.add(new_customer_account)
+    db.session.commit()
+    return jsonify({"message": "Customer account added successfully."}), 201
+
+@app.route('/customer_accounts/<int:id>', methods=['PUT'])
+def update_customer_account(id):
+    customer_account = CustomerAccount.query.get_or_404(id)
+    try:
+        customer_account_data = customer_account_schema.load(request.json)
+
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    customer_account.username = customer_account_data['username']
+    customer_account.password = customer_account_data['password']
+    db.session.commit()
+    return jsonify({"message": "Customer account updated successfully."}), 200
+
+@app.route('/customer_accounts', methods=['DELETE'])
+def delete_customer_account(id):
+    customer_account = CustomerAccount.query.get_or_404(id)
+    db.session.delete(customer_account)
+    db.session.commit()
+    return jsonify({"message": "Customer account deleted successfully."}), 200
+
+
+# Routes Separation---------------------Order Routes---------------------------------------------
+
+@app.route('/orders', methods=['GET'])
+def get_orders():
+    orders = Order.query.all()
+    return orders_schema.jsonify(orders)
+
+# TODO: Does the below need to have the foreign key associations with it?
+@app.route('/orders', methods=['POST'])
+def add_order():
+    try:
+        order_data = order_schema.load(request.json)
+        product_ids = order_data.get('product_ids', [])
+        products = Product.query.filter(Product.id.in_(product_ids)).all()
+
+        if not products:
+            return jsonify({"error": "No valid products found for the order."}), 400
+    
+        new_order = Order(
+                            order_date = order_data['order_date'],
+                            status = order_data['status'],
+                            close_date = order_data['close_date'],
+                            order_total = order_data['order_total'],
+                            )
+        new_order.products = products
+        new_order.calculate_total()
+
+        db.session.add(new_order)
+        db.session.commit()
+        return jsonify({"message": "Order added successfully."}), 201
+
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+# TODO: Does the below need to have the foreign key associations with it?
+@app.route('/orders/<int:id>', methods=['PUT'])
+def update_order(id):
+    order = Order.query.get_or_404(id)
+    try:
+        order_data = order_schema.load(request.json)
+        product_ids = order_data.get('product_ids',[])
+        if product_ids:
+            products = Product.query.filter(Product.id.in_(product_ids)).all()
+            if not products:
+                return jsonify({"error": "No valid products found for the update."}), 400
+            order.products = products
+
+        order.order_date = order_data.get('order_date', order.order_date)
+        order.status = order_data.get('status', order.status)
+        order.close_date = order_data.get('close_date', order.close_date)
+        order.calculate_total()
+
+        db.session.commit()
+        return jsonify({'message': 'Order updated successfully.'}), 200
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+
+@app.route('/orders/<int:id>', methods=['DELETE'])
+def delete_order(id):
+    order = Order.query.get_or_404(id)
+
+
+    db.session.delete(order)
+    db.session.commit()
+    return jsonify({'message': 'Order deleted successfully.'}), 200
+
+# Routes Separation ---------Product Routes-----------------------------------
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    product = Product.query.all()
+    return products_schema.jsonify(product)
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    try:
+        product_data = product_schema.load(request.json)
+
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    new_product = Product(name = product_data['name'],
+                          par_inventory = product_data['par_inventory'],
+                          current_inventory = product_data['current_inventory'],
+                          price = product_data['price'])
+    
+    db.session.add(new_product)
+    db.session.commit()
+    return jsonify({'message': 'Product added successfully'}), 201
+
+
+@app.route('/products/<int:id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get_or_404(id)
+    try:
+        product_data = product_schema.load(request.json)
+
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    product.name = product_data['name']
+    product.par_inventory = product_data['par_inventory']
+    product.current_inventory = product_data['current_inventory']
+    product.price = product_data['price']
+    db.session.commit()
+    return jsonify({'message': 'Product updated successfully.'}), 400
+
+
+@app.route('/products/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get_or_404(id)
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify({'message': 'Product deleted successfully.'})
+
+
+
+
+
 
 
 # Runs app in debug mode to allow for real time updating
